086b6100914f8d2b896ea06eed770bc75278bf61
shivamsingh-ml <shivam.29981@gmail.com>
2022-12-03 16:46:49 +0100

    Test

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name='snowflake',
+      version='0.1',
+      description='This is snowflake package',
+      author='Sahil Sharma',
+      packages=['snowflake'],
+      install_requires=['numpy','turtles'])
